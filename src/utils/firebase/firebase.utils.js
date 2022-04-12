e51ec8c6bd41655613382588cadc002a6f660425
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-04-12 13:20:09 -0400

    firsebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4m--GCpT96-NpqmqXJe36Wk4PZWmWt5M",
+  authDomain: "react-clothing-store-db1.firebaseapp.com",
+  projectId: "react-clothing-store-db1",
+  storageBucket: "react-clothing-store-db1.appspot.com",
+  messagingSenderId: "170319196547",
+  appId: "1:170319196547:web:109b6f60f23f52d40686b6",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  promt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
