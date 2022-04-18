e8dd89386dddc725d3efbad97816a95260887a63
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-04-18 14:24:31 -0400

    shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,22 @@
-import { useContext } from "react"
-import { ProductsContext } from "../../contexts/products.context"
-import ProductCard from "../../components/product-card/product-card.component"
-import "./shop.styles.scss"
-const Shop = () =>{
-    const {products} = useContext(ProductsContext)
-    return(
-        <div className="products-container">
-        {products.map((product)=>(
-         <ProductCard key={product.id} product = {product}/>
-        ))}
-        </div>
-    )
-}
+import { useContext } from 'react';
 
-export default Shop
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import './shop.styles.scss';
+
+const Shop = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((key) => {
+        const products = categoriesMap[key];
+        return <CategoryPreview key={key} title={key} products={products} />;
+      })}
+    </div>
+  );
+};
+
+export default Shop;
