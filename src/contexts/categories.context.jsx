2699576a559dac22b2fcc9614d4a90d1a965d962
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-04-21 16:05:47 -0400

    last touches

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from "react";
+
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const CategoriesContext = createContext({
