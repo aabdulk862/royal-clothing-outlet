412891a66b51c3f0aa8029fe34f715d3b94adcf8
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-04-21 15:31:51 -0400

    update

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,27 +1,37 @@
-import { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { CartContext } from '../../contexts/cart.context'
-import Button from '../button/button.component'
-import CartItem from '../cart-item/cart-item.component'
-import "./cart-dropdown.styles.scss"
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const CartDropdown= () =>{
-    const {cartItems} = useContext(CartContext)
-    const navigate = useNavigate()
+import { CartContext } from '../../contexts/cart.context';
 
-    const goToCheckoutHandler = () =>{
-        navigate('/checkout')
-    }
-    return(
-        <div className="cart-dropdown-container">
-            <div className="cart-items"> 
-            {cartItems.map((item)=>(
-            <CartItem key={item.id} cartItem={item}/>
-            ))}
-            </div>
-            <Button onClick ={goToCheckoutHandler} >GO TO CHECKOUT</Button>
-        </div>
-    )
-}
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-export default CartDropdown
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from './cart-dropdown.styles';
+
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
+
+  return (
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+    </CartDropdownContainer>
+  );
+};
+
+export default CartDropdown;
