10758afbd2ad260e2dfc1c219dba25818baa31cb
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-05-13 19:56:19 -0400

    category selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,13 +5,14 @@ import CategoriesPreview from '../categories-preview/categories-preview.componen
 import Category from '../category/category.component';
 import './shop.styles.scss';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(()=>{
     const getCategoriesMap = async () =>{
-        const categoryMap = await getCategoriesAndDocuments('categories')
-        dispatch(setCategoriesMap(categoryMap));
+        const categoriesArray = await getCategoriesAndDocuments('categories')
+        console.log(categoriesArray);
+        dispatch(setCategories(categoriesArray));
     }
     getCategoriesMap();
 },[]);
