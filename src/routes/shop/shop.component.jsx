9b9b4c89c6dab1011297d7fa7c8dbb68be3ca5ae
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-05-17 20:41:06 -0400

    redux thunk loading spinner

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,25 +1,23 @@
 import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import './shop.styles.scss'
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import './shop.styles.scss';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import { fetchCategoriesStartAsync } from '../../store/categories/category.action';
+
 const Shop = () => {
   const dispatch = useDispatch();
-  useEffect(()=>{
-    const getCategoriesMap = async () =>{
-        const categoriesArray = await getCategoriesAndDocuments('categories')
-        dispatch(setCategories(categoriesArray));
-    }
-    getCategoriesMap();
-},[]);
+
+  useEffect(() => {
+    dispatch(fetchCategoriesStartAsync());
+  }, []);
+
   return (
-        <Routes>
-          <Route index element ={<CategoriesPreview/>}/>
-          <Route path=':category' element ={<Category/>} />
-      </Routes>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
   );
 };
 
